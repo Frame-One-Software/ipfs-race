90b144822f2ae1d7cbd431dc6af88a53fd9f74ba
Christopher Powroznik <christopher@frameonesoftware.com>
2022-11-23 01:40:14 -0800

    urls and direct cids working

---
+++ src/resolve.ts
@@ -0,0 +1,186 @@
+import * as isIPFS from 'is-ipfs'
+import {defaultIpfsGateways, defaultIpnsGateways} from "./defaultGateways";
+import {isValidHttpUrl} from "./isValidHttpUrl";
+
+interface ResolveOptions {
+    ipfsGateways?: string[];
+    ipnsGateways?: string[];
+    defaultProtocolIfUnspecified: "ipfs" | "ipns";
+}
+
+const defaultResolveOptions: Required<ResolveOptions> = {
+    ipfsGateways: defaultIpfsGateways,
+    ipnsGateways: defaultIpnsGateways,
+    defaultProtocolIfUnspecified: "ipfs"
+}
+
+interface ResolveOutput {
+    response: Response;
+    urlResolvedFrom?: string;
+}
+
+/**
+ * Fetches data from an ipfs uri using multiple methods and resolving the first one that returns a value.
+ *
+ * @param {string} uri - the uri on ipfs that needs to be resolved, it can follow any of the following formats...
+ *      - <CID>
+ *      - <CID>/<path>
+ *      - ipfs/<CID>
+ *      - ipfs/<CID>/path
+ *      - ipns/<CID>
+ *      - ipns/<CID>/path
+ *      - http(s)://<gateway domain>/ipfs/<CID>
+ *      - http(s)://<gateway domain>/ipfs/<CID>/<path>
+ *      - http(s)://<gateway domain>/ipns/<CID>
+ *      - http(s)://<gateway domain>/ipns/<CID>/<path>
+ *      - ipfs://<CID>
+ *      - ipfs://<CID>/<path>
+ *      - ipns://<CID>
+ *      - ipns://<CID>/<path>
+ *      - https(s)://<regular url> - this will resolve an http request for a generic url
+ * @param options
+ */
+async function resolve(uri: string, options?: ResolveOptions): Promise<ResolveOutput> {
+
+    // merge the options with the default options
+    const _options: Required<ResolveOptions> = {
+        ...defaultResolveOptions,
+        ...(options || {})
+    };
+
+    let gatewaySuffix: string;
+    let protocol: "ipfs" | "ipns" = _options.defaultProtocolIfUnspecified;
+
+
+    // determine what type of uri was passed in
+    // CID or CID with path passed in directly
+    if (isIPFS.cid(uri) || isIPFS.cidPath(uri)) {
+        gatewaySuffix = `/${protocol}/${uri}`;
+    }
+
+    // ipfs path passed in
+    else if (isIPFS.ipfsPath(uri)) {
+        protocol = "ipns";
+        gatewaySuffix = `/${protocol}/${uri}`;
+    }
+
+    // ipns path passed in
+    else if (isIPFS.ipnsPath(uri)) {
+        protocol = "ipns";
+        gatewaySuffix = `/${protocol}/${uri}`;
+    }
+
+    // check to see if uri is a link to a gateway and ipfs
+    else if (isIPFS.ipfsUrl(uri) || isIPFS.ipnsUrl(uri)) {
+
+        // remove splits between "/"s until it is a valid ipfs or ipns path
+        let tmp = (uri as string).split("/");
+        let cid = uri;
+        do {
+            tmp.shift();
+            cid = `/${tmp.join("/")}`;
+        } while (tmp.length > 1 && !isIPFS.path(cid));
+
+        // determine the correct protocol
+        if (isIPFS.ipfsPath(cid)) {
+            protocol = "ipfs";
+        } else if (isIPFS.ipnsPath(cid)) {
+            protocol = "ipns";
+        }
+
+        gatewaySuffix = cid
+    }
+
+    // check to see if the link has the ipfs:// or ipns:// protocol/scheme
+    else if ((uri as string).startsWith("ipfs://") || (uri as string).startsWith("ipns://")) {
+
+        // check to see if the CID after the ipfs protocal/scheme is valid
+        const cidWithOptionalPath = (uri as string).substring(7);
+        if (!isIPFS.cid(cidWithOptionalPath) && !isIPFS.cidPath(cidWithOptionalPath)) {
+            throw new Error(`The uri (${uri}) passed in was malformed.`);
+        }
+
+        if ((uri as string).startsWith("ipfs://")) {
+            protocol = "ipfs";
+        } else if ((uri as string).startsWith("ipns://")) {
+            protocol = "ipns";
+        }
+
+        gatewaySuffix = `/${protocol}/${cidWithOptionalPath}`;
+    }
+
+    // check to see if the uri is just a regular url that we can request
+    else if (isValidHttpUrl(uri)) {
+        const response = await fetch(uri, {
+            method: "get",
+        });
+
+        if(response.status >= 300 || response.status < 200) {
+            throw new Error(`Url (${uri}) did not return a 2xx response and did not match a valid IPFS/IPNS format.`);
+        }
+
+        return {
+            response,
+            urlResolvedFrom: uri,
+        };
+    }
+
+    // throw an error because could not parse the uri
+    else {
+        throw new Error(`The uri (${uri}) passed in was malformed.`);
+    }
+
+    // determine if we are using ipfs or ipns gateways
+    const gateways: string[] = protocol === "ipfs" ? _options.ipfsGateways : _options.ipnsGateways;
+
+    // keep a list of all the abortControllers that will need to be called to abort the fetch calls
+    const abortControllers = Array(gateways.length);
+
+    // create a promise to each gateway
+    const promises = gateways.map(async (gatewayUrl, i): Promise<ResolveOutput> => {
+
+        // create an abort controller to stop the fetch requests when the first one is resolved.
+        const controller = new AbortController();
+        const signal = controller.signal;
+        abortControllers[i] = controller;
+
+        const urlResolvedFrom = `${gatewayUrl}${gatewaySuffix}`;
+        const response = await fetch(urlResolvedFrom, {
+            method: "get",
+            signal,
+        });
+
+        // remove the abort controller for the completed response
+        abortControllers[i] = undefined;
+
+        // check the response because a lot of gateways will do redirects and other checks which will not be compatible
+        // with this library
+        if(response.status >= 300 || response.status < 200) {
+            throw new Error(`Url (${urlResolvedFrom}) did not return a 2xx response`);
+        }
+
+        return {
+            response,
+            urlResolvedFrom
+        }
+    })
+
+    // get the first response from the gateway
+    const result = await Promise.any(promises);
+
+    // abort all the other requests
+    for (const abortController of abortControllers) {
+        if (abortController) {
+            abortController.abort()
+        }
+    }
+
+    return result;
+}
+
+export {
+    ResolveOptions,
+    defaultResolveOptions,
+    ResolveOutput,
+    resolve
+}
---
+++ src/test.ts
@@ -0,0 +1,44 @@
+import {resolve} from "./resolve";
+import { expect } from "chai";
+import {defaultIpfsGateways, defaultIpnsGateways} from "./defaultGateways";
+import {isValidHttpUrl} from "./isValidHttpUrl";
+
+describe("Tests the ipfs-race library.", () => {
+
+    const testCidWithNoPath = "QmaiJczLW9X1Gk7rQH7CgYCuquLZMbdWB6hhqznDBoqdLE" // https://etherscan.io/address/0x7239d51f7fc1b37ca350e3ad816dc32b2f8764d6#readContract
+    const testCidWithNoPathResult = {"name":"AFTRMATH Deluxe 001","description":"AFTRMATH is a thrill-seeking, cola-drinking, dune Surfer. Cares more about cactus than he cares about people.","decimals":0,"image":"ipfs://QmSJdVXDmPCU7L3houziZeqt6ATv58EvYxZkpuYamVprK7","animation_url":"https://ipfs.io/ipfs/QmeFFAYUsy4isdzszxCJgLd12UjYubeuHtwtkZ2BQt6vHG","legal":"https://www.lamoverse.com/nftlegal","attributes":[{"trait_type":"Rarity","value":"Deluxe"},{"trait_type":"Collection","value":"Cali Fallout"},{"trait_type":"Season","value":"Pre Season"},{"trait_type":"Mantra","value":"Opportunist Junker"},{"trait_type":"Hair","value":"Sun-Bleached Dreads"},{"trait_type":"Face","value":"Beard"},{"trait_type":"Clothing","value":"Casual Surfer"},{"trait_type":"Shoes","value":"Flip-Flops"}]};
+    const testCidWithPath = "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/0" // bored ape yacht club number 0 https://etherscan.io/address/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d#readContract
+    const testCidWithPathResult = {"image":"ipfs://QmRRPWG96cmgTn2qSzjwr2qvfNEuhunv6FNeMFGa9bx6mQ","attributes":[{"trait_type":"Earring","value":"Silver Hoop"},{"trait_type":"Background","value":"Orange"},{"trait_type":"Fur","value":"Robot"},{"trait_type":"Clothes","value":"Striped Tee"},{"trait_type":"Mouth","value":"Discomfort"},{"trait_type":"Eyes","value":"X Eyes"}]};
+
+    it("cid with no path", async () => {
+        const {response, urlResolvedFrom} = await resolve(testCidWithNoPath);
+        expect(await response.json(), urlResolvedFrom).to.deep.equal(testCidWithNoPathResult)
+    });
+
+    it("cid with path", async () => {
+        const {response, urlResolvedFrom} = await resolve(testCidWithPath);
+        expect(await response.json(), urlResolvedFrom).to.deep.equal(testCidWithPathResult)
+    })
+
+    it("gateway url with no path", async () => {
+        const {response, urlResolvedFrom} = await resolve(`https://ipfs.io/ipfs/${testCidWithNoPath}`);
+        expect(await response.json(), urlResolvedFrom).to.deep.equal(testCidWithNoPathResult)
+    });
+
+    it("gateway url with path", async () => {
+        const {response, urlResolvedFrom} = await resolve(`https://ipfs.io/ipfs/${testCidWithPath}`);
+        expect(await response.json(), urlResolvedFrom).to.deep.equal(testCidWithPathResult)
+    });
+
+    it("check that each default ipfs gateway to be a valid url", () => {
+        for (const url of defaultIpfsGateways) {
+            expect(isValidHttpUrl(url)).to.be.true;
+        }
+    })
+
+    it("check that each default ipns gateway to be a valid url", () => {
+        for (const url of defaultIpnsGateways) {
+            expect(isValidHttpUrl(url)).to.be.true;
+        }
+    })
+});
